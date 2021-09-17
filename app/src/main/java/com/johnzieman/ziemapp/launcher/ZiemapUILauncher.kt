e2dcce892835a61b03f8658e5a2496c25ed98bee
johnzieman <j.g.zokirov@gmail.com>
2021-09-17 22:16:22 +0500

    launcher-simple

--- app/src/main/java/com/johnzieman/ziemapp/launcher/ZiemapUILauncher.kt
+++ app/src/main/java/com/johnzieman/ziemapp/launcher/ZiemapUILauncher.kt
@@ -1,9 +1,19 @@
 package com.johnzieman.ziemapp.launcher
 
+import android.content.Intent
+import android.content.pm.ResolveInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "NerdLauncherActivity"
 
 class ZiemapUILauncher : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -12,5 +22,53 @@ class ZiemapUILauncher : AppCompatActivity() {
         setContentView(R.layout.activity_ziemap_uilauncher)
         recyclerView = findViewById(R.id.app_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        setaupAdapter()
+    }
+
+    private fun setaupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+        activities.sortWith(Comparator { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
+        Log.i(TAG, "Found ${activities.size} activities")
+        recyclerView.adapter = ActivityAdapter(activities)
+    }
+
+    private class ActivityAdapter(val activities: List<ResolveInfo>) :
+        RecyclerView.Adapter<ActivityHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater
+                .inflate(R.layout.item_icon_nama, parent, false)
+            return ActivityHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ActivityHolder, position: Int) {
+            val resolveInfo = activities[position]
+            holder.bindActivity(resolveInfo)
+        }
+
+        override fun getItemCount(): Int = activities.size
+    }
+
+    private class ActivityHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val nameTextView: TextView = itemView.findViewById(R.id.textView)
+        private val imageImageView: ImageView = itemView.findViewById(R.id.imageView)
+        private lateinit var resolveInfo: ResolveInfo
+
+        fun bindActivity(resolveInfo: ResolveInfo) {
+            this.resolveInfo = resolveInfo
+            val packageManager = itemView.context.packageManager
+            val appImage = resolveInfo.loadIcon(packageManager)
+            val appName = resolveInfo.loadLabel(packageManager).toString()
+            nameTextView.text = appName
+            imageImageView.setImageDrawable(appImage)
+        }
     }
 }
